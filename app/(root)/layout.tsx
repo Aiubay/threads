9d08f44b517d4b8e5e63aebe09681978ea169d36
aiubay6 <achmad.ubay@erajaya.com>
2023-09-08 16:27:18 +0700

    Edit button and stuff

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -28,19 +28,19 @@ export default function RootLayout({
         baseTheme: dark,
       }}
     >
-      <html lang="en">
-        {/* <> */}
-        <body className={inter.className} />
-        <Topbar />
-        <main className="flex flex-row">
-          <LeftSidebar />
-          <section className="main-container">
-            <div className="w-full max-w-4xl">{children}</div>
-          </section>
-          <RightSidebar />
-        </main>
-        <BottomBar />
-      </html>
+      {/* <html lang="en"> */}
+      {/* <> */}
+      <body className={inter.className} />
+      <Topbar />
+      <main className="flex flex-row">
+        <LeftSidebar />
+        <section className="main-container">
+          <div className="w-full max-w-4xl">{children}</div>
+        </section>
+        <RightSidebar />
+      </main>
+      <BottomBar />
+      {/* </html> */}
       {/* </> */}
     </ClerkProvider>
   );
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -52,3 +52,38 @@ export async function createThread({
     throw new Error(`Failed to create thread: ${error.message}`);
   }
 }
+
+export async function fetchPost(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  const postQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({
+      createdAt: "desc",
+    })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postQuery.exec();
+
+  const isNext = totalPostCount > skipAmount + posts.length;
+
+  return {
+    posts,
+    isNext,
+  };
+}
